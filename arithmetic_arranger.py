cef1c1b045041d6fd2cc6c443c69b8b14fb67752
Trepix <pau.trepat.segura@gmail.com>
2023-03-11 00:16:37 +0100

    Add clause guard on parsing for incorrect operator case

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,11 @@
 def arithmetic_arranger(problems):
     if len(problems) > 5:
         return "Error: Too many problems."
+
+    for problem in problems:
+        first, operation, second = tuple(problem.split())
+
+        if operation != "+" and operation != '-':
+            return "Error: Operator must be '+' or '-'."
+
     return None
