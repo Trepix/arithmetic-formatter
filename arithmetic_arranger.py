f2d16ce3080925fea90c4f2c9588d335a109ba14
Trepix <pau.trepat.segura@gmail.com>
2023-03-11 01:18:24 +0100

    #2 Push parsing and guard clause into method

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,17 +1,34 @@
-def arithmetic_arranger(problems):
+def parse(problems):
     if len(problems) > 5:
-        return "Error: Too many problems."
+        return None, "Error: Too many problems."
 
+    result = []
     for problem in problems:
         first, operation, second = tuple(problem.split())
-
         if operation != "+" and operation != '-':
-            return "Error: Operator must be '+' or '-'."
+            return None, "Error: Operator must be '+' or '-'."
 
         if not (first.isdigit() and second.isdigit()):
-            return "Error: Numbers must only contain digits."
+            return None, "Error: Numbers must only contain digits."
 
         if len(first) > 4 or len(second) > 4:
-            return "Error: Numbers cannot be more than four digits."
+            return None, "Error: Numbers cannot be more than four digits."
+
+        result.append(ArithmeticOperation(first, operation, second))
+
+    return result, None
+
+
+
+def arithmetic_arranger(problems):
+    arithmetic_operations, error = parse(problems)
+    if error:
+        return error
 
     return "\n".join(["  3801      123", "-    2    +  49", "------    -----"])
+
+class ArithmeticOperation:
+    def __init__(self, first, operation, second):
+        self.operation = operation
+        self.first = first
+        self.second = second
